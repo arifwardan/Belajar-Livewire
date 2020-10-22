97f7fc921e5e804c96e2ce2f0fdf2e5232b64b31
arifwardan <arifwardan.id@gmail.com>
2020-10-22 08:34:56 +0700

    added

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/tes', function () {
+    return view('dashboard.core');
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
