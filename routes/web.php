3983aa766b14bd2653fa6cbff00e588700ff3b75
arifwardan <arifwardan.id@gmail.com>
2020-10-25 20:48:17 +0700

    Install Ulang

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tes', function () {
-    return view('dashboard.dashboard');
-});
-
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
